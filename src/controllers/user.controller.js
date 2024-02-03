d3a8f1372073fe0395c80bffec9d9391d2b66a1c
sahil_saif <sahilsaif2002@gmail.com>
2024-02-03 15:19:52 +0530

    login functionality done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import fs from 'fs'
+const generateAccessAndRefreshToken = async(userId) => {
 
+    const user = await User.findById(userId);
 
+    const accessToken = await user.generateAccessToken()
+    const refreshToken = await user.generateRefreshToken()
+
+    user.refreshToken=refreshToken
+    user.save({validateBeforeSave:false})
+
+    return {accessToken,refreshToken}
+
+}
 
 const registerUser=asyncHandler(async(req, res)=>{
     //# steps :
@@ -19,24 +31,30 @@ const registerUser=asyncHandler(async(req, res)=>{
     //* 9. return res
 
     const {username,email,password,fullName} = req.body
-
-    if([username,email,password,fullName].some((field)=>field?.trim()==="")){
-        throw new ApiError(400,"Few fields are empty. All fields are required ! ")
-    }
-    const existedUser=await User.findOne({
-        $and:[{email},{username}]
-    })
-
-    if (existedUser) {
-        throw new ApiError(401,"An user already exists with either this username or email")
-    }
-    // console.log(req.files);
-
+    
     const avatarLocalPath=req.files.avatar?.length>0? req.files.avatar[0].path : false
     const coverImageLocalPath=req.files.coverImage?.length>0? req.files.coverImage[0].path : false
     if(!avatarLocalPath) {
         throw new ApiError(400,"Avatar is required")
     }
+
+    if([username,email,password,fullName].some((field)=>field?.trim()==="")){
+        if (avatarLocalPath) fs.unlinkSync(avatarLocalPath)
+        if (coverImageLocalPath) fs.unlinkSync(coverImageLocalPath)
+        throw new ApiError(400,"Few fields are empty. All fields are required ! ")
+    }
+    
+    
+    const existedUser=await User.findOne({
+        $and:[{email},{username}]
+    })
+    if (existedUser) {
+        if (avatarLocalPath) fs.unlinkSync(avatarLocalPath)
+        if (coverImageLocalPath) fs.unlinkSync(coverImageLocalPath)
+        throw new ApiError(401,"An user already exists with either this username or email")
+    }
+    // console.log(req.files);
+
     const avatar=await uploadOnCloudinary(avatarLocalPath)
     if(!avatar) {
         throw new ApiError(400,"Avatar is required")
@@ -68,6 +86,60 @@ const registerUser=asyncHandler(async(req, res)=>{
     )
 })
 
+
+const loginUser=asyncHandler(async (req, res) => {
+    //* 1.extract data from  req body -> data
+    //* 2.username or email
+    //* 3.find the user
+    //* 4.password check
+    //* 5.generate access and refresh token
+    //* 6.send res and add cookie
+
+    const {email,password,username} =  req.body
+    console.log(email);
+    if(!(email||username)) {
+        throw new ApiError(400,"username or email required must")
+        // if (!username) {
+        // }
+    }
+
+    const user=await User.findOne({
+        $or: [{email},{username}]
+    })
+
+    if (!user) {
+        throw new ApiError(404,"User not found ")
+        
+    }
+
+    const isPasswordValid=await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401,"incorrect password !")
+    }
+
+    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
+    const loggedInUser=await User.findById(user._id)
+    .select("-password -refreshToken")
+
+    const options={
+        httpOnly: true,
+        secure:true
+    }
+
+    res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new ApiResponse(200,loggedInUser,"user logged in successfully")
+    )
+})
+
+const logoutUser=asyncHandler(async(req,res) => {
+
+})
 export {
-    registerUser
+    registerUser,
+    loginUser,
+    logoutUser
+
 }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 
 const router=Router()
 
@@ -14,5 +14,7 @@ router.route("/register").post(upload.fields([
         maxCount:1
     }
 ]),registerUser)
+
+router.route("/login").post(loginUser)
 
 export default router
